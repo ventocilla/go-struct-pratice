e30725083b31c08a743d5f35b3fc639ae03796d8
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-03-28 17:40:13 -0300

    fixed json output

--- note/note.go
+++ note/note.go
@@ -10,18 +10,18 @@ import (
 )
 
 type Note struct {
-	title     string
-	content   string
-	createdAt time.Time
+	Title     string
+	Content   string
+	CreatedAt time.Time
 }
 
 func (note Note) Display() {
-	fmt.Printf("You note titled %v has the following content:\n\n%v\n\n", note.title, note.content)
+	fmt.Printf("You note titled %v has the following content:\n\n%v\n\n", note.Title, note.Content)
 }
 
 func (note Note) Save() error {
-	fileName := strings.ReplaceAll(note.title, " ", "_")
-	fileName = strings.ToLower(fileName)
+	fileName := strings.ReplaceAll(note.Title, " ", "_")
+	fileName = strings.ToLower(fileName) + ".json"
 	json, err := json.Marshal(note)
 
 	if err != nil {
@@ -36,8 +36,8 @@ func New(title, content string) (Note, error) {
 		return Note{}, errors.New("invalid input")
 	}
 	return Note{
-		title:     title,
-		content:   content,
-		createdAt: time.Now(),
+		Title:     title,
+		Content:   content,
+		CreatedAt: time.Now(),
 	}, nil
 }
