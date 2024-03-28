bad4021f29bc3a4da24b5f36eb2738f45b5a9df7
Leonardo Ventocilla <ventocilla.leo@gmail.com>
2024-03-28 17:02:22 -0300

    encoding json content

--- note/note.go
+++ note/note.go
@@ -1,6 +1,7 @@
 package Note
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"os"
@@ -18,10 +19,16 @@ func (note Note) Display() {
 	fmt.Printf("You note titled %v has the following content:\n\n%v\n\n", note.title, note.content)
 }
 
-func (note Note) Save() {
+func (note Note) Save() error {
 	fileName := strings.ReplaceAll(note.title, " ", "_")
 	fileName = strings.ToLower(fileName)
-	os.WriteFile(fileName)
+	json, err := json.Marshal(note)
+
+	if err != nil {
+		return err
+	}
+
+	return os.WriteFile(fileName, json, 0644)
 }
 
 func New(title, content string) (Note, error) {
